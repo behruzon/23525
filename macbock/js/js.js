0bc7b31a2f44e982df71ead7def8ae5781831a64
behruzon <113119434+behruzon@users.noreply.github.com>
2022-12-17 20:12:48 +0500

    Add files via upload

---
+++ macbock/js/js.js
@@ -0,0 +1,74 @@
+let btn1 = document.querySelector(".btn-common1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let p = document.querySelector(".laptop_price");
+let color = document.querySelector(".color");
+let white_btn = document.querySelector(".btn-1");
+let grey_btn = document.querySelector(".btn-2");
+let img = document.querySelector("img");
+let src1 = "./img/laptop.png";
+let src2 = "./img/imp2.png";
+
+let rem_gray = (btn) => {
+  btn.classList.remove("white_back");
+  btn.classList.add("blue_back");
+  btn.style.color = "white";
+};
+let rem_white = (btn) => {
+  btn.classList.remove("blue_back");
+  btn.classList.add("white_back");
+  btn.style.color = "gray";
+};
+
+grey_btn.onclick = () => {
+  img.setAttribute("src", src1);
+  rem_gray(grey_btn);
+  rem_white(white_btn);
+  color.innerHTML = "Space Gray";
+};
+white_btn.onclick = () => {
+  img.setAttribute("src", src2);
+  rem_gray(white_btn);
+  rem_white(grey_btn);
+  color.innerHTML = "White";
+};
+
+let rem = (btn) => {
+  btn.classList.remove("btn-common1");
+};
+
+btn1.onclick = () => {
+  p.innerHTML = "$1,999";
+  btn1.classList.add("btn-common1");
+  rem(btn2);
+  rem(btn3);
+  rem(btn4);
+};
+btn2.onclick = () => {
+  p.innerHTML = "$2,199";
+  btn2.classList.add("btn-common1");
+  rem(btn1);
+  rem(btn3);
+  rem(btn4);
+};
+btn3.onclick = () => {
+  p.innerHTML = "$2,599";
+  btn3.classList.add("btn-common1");
+  rem(btn1);
+  rem(btn2);
+  rem(btn4);
+};
+btn4.onclick = () => {
+  p.innerHTML = "$3,199";
+  btn4.classList.add("btn-common1");
+  rem(btn1);
+  rem(btn3);
+  rem(btn2);
+};
+
+
+
+
+
+
